f61a7cbdb0bb36288417c8e08fecfc803c35bf0b
15huangtimothy <timhhuang@gmail.com>
2020-06-01 19:53:55 -0700

    nginx fix

--- udagram-api-users/src/server.ts
+++ udagram-api-users/src/server.ts
@@ -32,12 +32,12 @@ import { V0_USER_MODELS } from "./controllers/v0/model.index";
         })
     );
 
-    app.use("/", IndexRouter);
+    app.use("/api/v0/", IndexRouter);
 
     // Root URI call
-    // app.get("/", async (req, res) => {
-    //     res.send("/api/v0/");
-    // });
+    app.get("/", async (req, res) => {
+        res.send("/api/v0/");
+    });
 
     // Start the Server
     app.listen(port, () => {
